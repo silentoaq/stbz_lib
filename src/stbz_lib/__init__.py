27989c8a8883d9657124a58556edce07855c8eac
Kac TT <88936771+silentoaq@users.noreply.github.com>
2025-06-19 18:28:26 +0800

    feat(mouse ): 建立 mouse 功能函數骨架  (#2)

--- src/stbz_lib/__init__.py
+++ src/stbz_lib/__init__.py
@@ -3,5 +3,17 @@ stbz_lib 對外 API。
 """
 
 from ._kb import kb_block, kb_hold, kb_tap, kb_unblock
+from ._mouse import mouse_block, mouse_hold, mouse_move, mouse_pos, mouse_tap, mouse_unblock
 
-__all__ = ["kb_block", "kb_unblock", "kb_tap", "kb_hold"]
+__all__ = [
+    "kb_block",
+    "kb_unblock",
+    "kb_tap",
+    "kb_hold",
+    "mouse_block",
+    "mouse_unblock",
+    "mouse_tap",
+    "mouse_hold",
+    "mouse_pos",
+    "mouse_move",
+]
