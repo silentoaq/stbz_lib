33f13d3133c226c5c8487b5badce0b3443935796
Kac TT <88936771+silentoaq@users.noreply.github.com>
2025-06-20 15:33:46 +0800

    feat(capture): 完成 capture 功能 (#6)
    * feat(core): 添加 gdi 相關結構
    
    * feat(capture): 完成 capture 功能

--- src/stbz_lib/__init__.py
+++ src/stbz_lib/__init__.py
@@ -2,18 +2,23 @@
 stbz_lib 對外 API。
 """
 
+from ._capture import capture
 from ._kb import kb_block, kb_hold, kb_tap, kb_unblock
 from ._mouse import mouse_block, mouse_hold, mouse_move, mouse_pos, mouse_tap, mouse_unblock
 
 __all__ = [
+    # 鍵盤
     "kb_block",
     "kb_unblock",
     "kb_tap",
     "kb_hold",
+    # 滑鼠
     "mouse_block",
     "mouse_unblock",
     "mouse_tap",
     "mouse_hold",
     "mouse_pos",
     "mouse_move",
+    # 截圖
+    "capture",
 ]
